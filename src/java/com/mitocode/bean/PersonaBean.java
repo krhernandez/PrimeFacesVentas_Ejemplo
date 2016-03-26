8f2987239eb2b0e41ebc0d4d90ec5f20c1e0e18c
HBK <>
2016-03-26 00:11:07 -0500

    Agregando metodos para listar, modificar y eliminar elementos de la BD...

--- src/java/com/mitocode/bean/PersonaBean.java
+++ src/java/com/mitocode/bean/PersonaBean.java
@@ -2,18 +2,20 @@ package com.mitocode.bean;
 
 import com.mitocode.dao.PersonaDAO;
 import com.mitocode.model.Persona;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 /**
  *
  * @author HBK
  */
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class PersonaBean {
 
     private Persona persona = new Persona();
+    private List<Persona> lstPersonas;
 
     public Persona getPersona() {
         return persona;
@@ -28,6 +30,60 @@ public class PersonaBean {
         try{
             dao = new PersonaDAO();
             dao.registrar(persona);
+        }catch (Exception e){
+            throw e;
+        }
+    }
+    
+    public void listar() throws Exception{
+        PersonaDAO dao;
+        try{
+            dao = new PersonaDAO();
+            lstPersonas = dao.listar();
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    public List<Persona> getLstPersonas() {
+        return lstPersonas;
+    }
+
+    public void setLstPersonas(List<Persona> lstPersonas) {
+        this.lstPersonas = lstPersonas;
+    }
+    
+    public void leerID(Persona per) throws Exception{
+        PersonaDAO dao;
+        Persona temp;
+        try{
+            dao = new PersonaDAO();
+            temp = dao.leerID(per);
+            if(temp != null){
+                this.persona = temp;
+            }
+        }catch (Exception e){
+            throw e;
+        }
+    }
+    
+    public void modificar() throws Exception{
+        PersonaDAO dao;
+        try{
+            dao = new PersonaDAO();
+            dao.modificar(persona);
+            this.listar();
+        }catch (Exception e){
+            throw e;
+        }
+    }
+    
+    public void eliminar(Persona per) throws Exception{
+        PersonaDAO dao;
+        try{
+            dao = new PersonaDAO();
+            dao.eliminar(per);
+            this.listar();
         }catch (Exception e){
             throw e;
         }
--- src/java/com/mitocode/dao/PersonaDAO.java
+++ src/java/com/mitocode/dao/PersonaDAO.java
@@ -2,6 +2,9 @@ package com.mitocode.dao;
 
 import com.mitocode.model.Persona;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -22,4 +25,83 @@ public class PersonaDAO extends DAO {
             this.Cerrar();
         }
     }
+
+    public List<Persona> listar() throws Exception {
+        List<Persona> lista;
+        ResultSet rs;
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCn().prepareStatement("SELECT * FROM persona");
+            rs = st.executeQuery();
+            lista = new ArrayList();
+            while (rs.next()) {
+                Persona per = new Persona();
+                per.setCodigo(rs.getInt("codigo"));
+                per.setNombre(rs.getString("nombre"));
+                if (rs.getString("sexo").equals("M")) {
+                    per.setSexo("MASCULINO");
+                } else {
+                    per.setSexo("FEMENINO");
+                }
+
+                lista.add(per);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+        return lista;
+    }
+
+    public Persona leerID(Persona per) throws Exception {
+        Persona pers = null;
+        ResultSet rs;
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCn().prepareStatement("SELECT * FROM persona WHERE codigo = ?");
+            st.setInt(1, per.getCodigo());
+            rs = st.executeQuery();
+            while (rs.next()) {
+                pers = new Persona();
+                pers.setCodigo(rs.getInt("codigo"));
+                pers.setNombre(rs.getString("nombre"));
+                pers.setSexo(rs.getString("sexo"));
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+        return pers;
+    }
+    
+    public void modificar(Persona per) throws Exception {
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCn().prepareStatement("UPDATE persona SET nombre = ?, sexo = ? WHERE codigo = ?");
+            st.setString(1, per.getNombre());
+            st.setString(2, per.getSexo());
+            st.setInt(3, per.getCodigo());
+            st.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+    
+    public void eliminar(Persona per) throws Exception {
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCn().prepareStatement("DELETE FROM persona WHERE codigo = ?");
+            st.setInt(1, per.getCodigo());
+            st.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+
 }
